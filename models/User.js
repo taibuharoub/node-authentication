f1cee762880c1cecd0b979c7d0d9e88da5c3fa82
Taibu Haroub <haroubtaibu@gmail.com>
2021-08-02 20:04:55 +0300

    Setup Database Encryption

--- models/User.js
+++ models/User.js
@@ -1,10 +1,20 @@
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const secret = "iysjkdkdlke773hdh";
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     email: String,
-    password: String
-}, { timestamps: true });
+    password: String,
+  },
+  { timestamps: true }
+);
 
+//encrypt the entire db
+// userSchema.plugin(encrypt, { secret: secret });
+//encrypt a single field
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 module.exports = mongoose.model("User", userSchema);
