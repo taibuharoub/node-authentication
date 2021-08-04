056d17454cdd07748678afa4a28745a300de6123
Taibu Haroub <haroubtaibu@gmail.com>
2021-08-04 21:16:55 +0300

    salting and hashing passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,45 +1,51 @@
-const md5 = require("md5");
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
+const saltRounds = 10;
+
 exports.login = (req, res, next) => {
-    res.status(200).render("login")
-}
+  res.status(200).render("login");
+};
 
 exports.register = (req, res, next) => {
-    res.status(200).render("register")
-}
+  res.status(200).render("register");
+};
 
 exports.postRegister = (req, res, next) => {
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
     const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
+      email: req.body.username,
+      password: hash,
+    });
 
-    newUser.save(err => {
-        if(err) {
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
-    })
-}
+    newUser.save((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+  });
+};
 
 exports.postLogin = (req, res, next) => {
-    const username = req.body.username;
-    const password = md5(req.body.password);
-    
-    User.findOne({email: username}, (err, foundUser) => {
-        if (err) {
-            console.log(err);
-            res.redirect("/login")
-        } else {
-            if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                }
-            }else {
-                res.redirect("/login")
-            }
-        }
-    })
-}
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username }, (err, foundUser) => {
+    if (err) {
+      console.log(err);
+      res.redirect("/login");
+    } else {
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function (err, result) {
+          if (result === true) {
+            res.render("secrets");
+          }
+        });
+      } else {
+        res.redirect("/login");
+      }
+    }
+  });
+};
